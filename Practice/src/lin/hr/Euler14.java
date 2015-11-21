6e607b9f20df363e833b86e68f60f549649d8345
ankcrimson <AnkurPrakash.Srivastava@gmail.com>
2015-11-21 13:57:11 -0800

    updating from local history

--- Practice/src/lin/hr/Euler14.java
+++ Practice/src/lin/hr/Euler14.java
@@ -11,17 +11,64 @@ import java.util.List;
 import java.util.Map;
 
 public class Euler14 {
-
+	static final int lim=5000001;
 	 public static void main(String[] args) throws Exception {
-		 
+		 int[] cache=new int[lim];
+		 int[] ans=new int[lim];
+		 int max=0;
+		 int maxpos=0;
 		 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		 int t=Integer.parseInt(br.readLine());
-		 for(int i=1;i<=t;i++)
+		List<Integer> nums=new LinkedList<Integer>();
+		 for(int i=1;i<lim;i++)
 		 {
-			 int curr=Integer.parseInt(br.readLine());
-		
+			 nums.clear();
+			 int len=1;
+			 int num=i;
+			 while(num>1)
+			 {
+				
+				 if(num<cache.length&&cache[num]>0)
+				 {
+					 nums.add(num);
+					 len+=cache[num];
+					 break;
+				 }
+				 if(num%2==0)
+					 num=num/2;
+				 else
+					 num=3*num+1;
+				 
+				 nums.add(num);
+				 len++;
+			 }
+			 int val=2;
+			 if(nums.size()>1&&cache[nums.size()-1]>1)
+			 val=cache[nums.size()-1];
+			 for(int j=nums.size()-1;j>=0;j--)
+			 {
+				 if(nums.get(j)<cache.length&&nums.get(j)>0)
+				 cache[nums.get(j)]=val;
+				 val++;
+			 }
+//			 if(i==3)
+//				 {
+//				 System.out.println(nums);
+//				 System.out.println(cache[10]);
+//				 }
+			 cache[i]=len;
+			 if(len>=max)
+			 {
+				 max=len;
+				 maxpos=i;
+			 }
+			 ans[i]=maxpos;
 			 
 		 }
-		 
+		 for(int j=0;j<t;j++)
+		 {
+			 int curr=Integer.parseInt(br.readLine());
+			 System.out.println(ans[curr]);
+		 }
 	}
 }
