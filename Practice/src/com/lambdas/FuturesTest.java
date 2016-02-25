3cde03a0f5806c8592475b7e2e525f07c4afcf7b
Ankur Srivastava <Ankur.Srivastava@nike.com>
2016-02-24 21:56:36 -0800

    adding future example

---
+++ Practice/src/com/lambdas/FuturesTest.java
@@ -0,0 +1,63 @@
+package com.lambdas;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+
+class Factorial implements Runnable {
+
+  long x;
+
+  public Factorial(long x) {
+    this.x = x;
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Factorial of " + x + " is " + factorial(x));
+    try {
+      Thread.sleep(2000);
+    } catch (Exception ex) {
+    }
+  }
+
+  public BigInteger factorial(long num) {
+    BigInteger ans = new BigInteger(num + "");
+    while (num-- > 2) {
+      ans = ans.multiply(new BigInteger("" + num));
+    }
+    return ans;
+  }
+
+}
+
+
+public class FuturesTest {
+
+  ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(5);
+
+  public static void main(String[] args) throws InterruptedException, ExecutionException {
+    FuturesTest futuresTest = new FuturesTest();
+
+    try (InputStreamReader isr = new InputStreamReader(System.in); BufferedReader br = new BufferedReader(isr);) {
+      String num = null;
+      while ((num = br.readLine()) != null) {
+        if (num.length() == 0)
+          break;
+        int n = Integer.parseInt(num);
+        Factorial fact = new Factorial(n);
+        futuresTest.scheduledThreadPoolExecutor.submit(fact);
+      }
+    } catch (IOException ioexec) {
+      System.out.println("IOException :( :" + ioexec);
+    }
+    System.out.println("Quitting, Bye..");
+
+    futuresTest.scheduledThreadPoolExecutor.shutdown();
+  }
+
+}
