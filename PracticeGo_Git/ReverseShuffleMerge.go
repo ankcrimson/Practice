e99b74bc53f89e79db4504dfde745ca90c3b8326
Ankur Srivastava <Ankur.Srivastava@nike.com>
2016-05-15 15:23:50 -0700

    updated

--- PracticeGo_Git/ReverseShuffleMerge.go
+++ PracticeGo_Git/ReverseShuffleMerge.go
@@ -1,81 +1,100 @@
-// aahaxxxhxhxxah
 package main
 
 import "fmt"
 
-//find minimum and return reduced buffer
-func bufferOperations(buffer []byte) (byte, []byte) {
-	var min byte
-	pos := 0
-	if len(buffer) > 0 {
-		min = buffer[0]
-	}
-	for i := 1; i < len(buffer); i++ {
-		if buffer[i] < min {
-			min = buffer[i]
-			pos = i
+func currNeededCalc(charCount [26]int) int {
+	currNeeded := 0
+	for j := 0; j < 26; j++ {
+		if charCount[j] > 0 {
+			currNeeded = j
+			break
 		}
 	}
-	return min, buffer[pos+1:]
+	return currNeeded
+}
+
+func findLexographicSmallest(buffer []byte, curr byte, need [26]int) ([]byte, [26]int) {
+	var retString []byte
+	var charCount [26]int
+	for i := range buffer {
+		charCount[buffer[i]-'a']++
+	}
+
+	pos := 0
+	for i := 0; i < 26; i++ {
+		if charCount[i] != 0 && i < int(curr-'a') && need[i] > 0 {
+			//scan buffer for this character
+
+			for j := pos; j < len(buffer); j++ {
+				if byte(i+'a') == buffer[j] && need[i] > 0 {
+					retString = append(retString, byte(i+'a'))
+					need[i]--
+					pos = j + 1
+					//break
+				}
+			}
+
+		}
+	}
+	fmt.Println("Buffer=", string(buffer), "Return=", string(retString), "curr=", string(curr))
+	return retString, need
 }
 
 func main() {
-	var ans string
-	var str string
-	fmt.Scanf("%s", &str)
-	var needed [26]int
-	byteArray := []byte(str)
+	var inputStr string
+	fmt.Scanf("%s", &inputStr)
+	byteArray := []byte(inputStr)
+	var charCount [26]int
 	for i := range byteArray {
-		needed[byteArray[i]-'a']++
+		charCount[byteArray[i]-'a']++
 	}
-	for i := 0; i < 26; i++ {
-		needed[i] /= 2
-	}
+	//charCount will contain how many are needed
+	var needed [26]int
 
-	var buffer []byte
-	var passed [26]int
+	for i := range charCount {
+		needed[i] = charCount[i] / 2
+	}
 	fmt.Println(needed)
-
-	//start parsing each char from right
+	//how many passed already
+	var passed [26]int
+	var buffer []byte
+	ans := ""
+	//scan from right
 	for i := len(byteArray) - 1; i >= 0; i-- {
-		//loop over each char in chars counts from a-z
-		curr := byteArray[i]
-		passed[curr-'a']++
-		var minumumCharNeeded byte
-		for j := 0; j < 26; j++ {
-			if needed[j] > 0 {
-				minumumCharNeeded = byte('a' + j)
-				break
-			}
-		}
-		if curr == minumumCharNeeded {
-			ans += string(curr)
-			needed[curr-'a']--
-			passed[curr-'a']--
-			buffer = buffer[:0]
-		} else if passed[curr-'a'] > needed[curr-'a'] {
-			// fmt.Println(curr, buffer)
-			for len(buffer) > 0 {
-				var min byte
-				min, buffer = bufferOperations(buffer)
-				// fmt.Println(string(buffer), min)
-
-				if needed[min-'a'] > 0 && min < curr {
-					ans += string(min)
-					needed[min-'a']--
-					passed[min-'a']--
-				}
-			}
-			buffer = buffer[:0]
-			//process current character
-			if needed[curr-'a'] > 0 {
-				ans += string(curr)
-				needed[curr-'a']--
-				passed[curr-'a']--
-			}
+		//scan for smallest, if smallest needed found then add to ans, decrease need
+		currentNeeded := currNeededCalc(needed)
+		if currentNeeded == int(byteArray[i]-'a') {
+			ans += string(byteArray[i])
+			needed[currentNeeded]--
+			var tmpBuffer []byte
+			fmt.Println("buffer=", string(buffer), "added=", string(byteArray[i]))
+			buffer = tmpBuffer
 		} else {
-			buffer = append(buffer, curr)
+			//if smallest not found, check for criticality,
+			if needed[byteArray[i]-'a'] >= charCount[byteArray[i]-'a']-passed[byteArray[i]-'a'] {
+				//get the buffer and find the minimum possible string based on needed
+				var currBytes []byte
+				currBytes, needed = findLexographicSmallest(buffer, byteArray[i], needed)
+				ans += string(currBytes)
+
+				var tmpBuffer []byte
+				buffer = tmpBuffer
+
+				if needed[byteArray[i]-'a'] >= charCount[byteArray[i]-'a']-passed[byteArray[i]-'a'] {
+					ans += string(byteArray[i])
+				} else {
+					buffer = append(buffer, byteArray[i])
+				}
+
+			} else {
+				buffer = append(buffer, byteArray[i])
+			}
+			//if critical, scan buffer and get the smallest string before adding the first critical char
+
+			// else add to passed and move on
 		}
+		passed[byteArray[i]-'a']++
+
 	}
 	fmt.Println(ans)
 }
